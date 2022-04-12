7a0c889609a44af8de766012db92168f13d569d4
Josh <adesanyajoshua@ymail.com>
2022-04-12 07:12:32 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
-import HomePage from "./components/HomePage";
-import GetTickets from "./components/GetTickets";
-import Setting from "./components/Setting/Settings";
-import AdminChange from "./components/AdminChange/AdminChange";
-
 import "./App.css";
 import "./App1.css";
 
@@ -13,10 +8,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/home" element={<Home />} />
-        <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/get-tickets" element={<GetTickets />} />
-        <Route exact path="/admin/add" element={<AdminChange />} />
-        <Route exact path="/admin" element={<Setting />} />
       </Routes>
     </BrowserRouter>
   );
